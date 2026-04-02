54fd89c450d517260b912a5206f8e316b37f62c7
Your Name <your.email@example.com>
2026-04-02 17:14:39 +0800

    功能更新：改进版本号管理，在编译过程中生成版本号

--- include/version_manager.h
+++ include/version_manager.h
@@ -4,8 +4,6 @@
 #include <string>
 #include <fstream>
 #include <sstream>
-#include <ctime>
-#include <iomanip>
 
 class VersionManager {
 private:
@@ -14,63 +12,21 @@ private:
     
 public:
     static std::string getVersion() {
-        std::time_t now = std::time(nullptr);
-        std::tm* localTime = std::localtime(&now);
-        
-        char dateTimeBuffer[20];
-        std::strftime(dateTimeBuffer, sizeof(dateTimeBuffer), "%Y%m%d%H%M", localTime);
-        std::string dateTimeStr = dateTimeBuffer;
-        
-        int serialNumber = getAndIncrementSerialNumber(dateTimeStr.substr(0, 8));
-        
-        std::ostringstream versionStream;
-        versionStream << BASE_VERSION << "_" << dateTimeStr << "_" << std::setw(3) << std::setfill('0') << serialNumber;
-        
-        return versionStream.str();
-    }
-    
-private:
-    static int getAndIncrementSerialNumber(const std::string& date) {
-        int serialNumber = 1;
+        std::string version = BASE_VERSION;
         
         std::ifstream configFile(CONFIG_FILE);
         if (configFile.is_open()) {
             std::string line;
             while (std::getline(configFile, line)) {
-                if (line.substr(0, 8) == date) {
-                    serialNumber = std::stoi(line.substr(9)) + 1;
+                if (line.substr(0, 8) == "version=") {
+                    version = line.substr(8);
                     break;
                 }
             }
             configFile.close();
         }
         
-        std::ofstream outFile(CONFIG_FILE);
-        if (outFile.is_open()) {
-            std::ifstream inFile(CONFIG_FILE);
-            if (inFile.is_open()) {
-                std::string line;
-                bool updated = false;
-                while (std::getline(inFile, line)) {
-                    if (line.substr(0, 8) == date) {
-                        outFile << date << " " << serialNumber << std::endl;
-                        updated = true;
-                    } else {
-                        outFile << line << std::endl;
-                    }
-                }
-                inFile.close();
-                
-                if (!updated) {
-                    outFile << date << " " << serialNumber << std::endl;
-                }
-            } else {
-                outFile << date << " " << serialNumber << std::endl;
-            }
-            outFile.close();
-        }
-        
-        return serialNumber;
+        return version;
     }
 };
 
